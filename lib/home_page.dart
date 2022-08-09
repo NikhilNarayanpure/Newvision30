ce0df4cf18cea78d84d55f92d9a372897f8f3436
nikhil.narayanpure <nikhil.narayanpure@newvisionsoftware.io>
2022-08-09 11:17:22 +0530

    Day 2 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "Nikhil";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("NewVision"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text(
+              "Welcome to Newvision$days $name Hope You will have great journey"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
